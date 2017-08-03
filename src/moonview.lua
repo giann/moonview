bf7dfc59a0a7a804833a589882224e5c7046437f
Benoit Giannangeli <giann008@gmail.com>
2017-08-03 10:06:47 +0200

    Model which will trigger rendering of View when modified

--- src/moonview.lua
+++ src/moonview.lua
@@ -4,28 +4,61 @@ local document = global.document
 local q = require("moonview.helpers").q
 local lustache = require("lustache")
 local Class = require("hump.class")
+local Signal = require("hump.signal")
 
+-- View
 local View = Class {
 
     init = function(self, options)
         self.template = options and options.template or nil
         self.target   = options and options.target or nil
         self.model    = options and options.model or {}
+
+        self.modelUpdate = options and options.modelUpdate or function() end
+
+        Signal.register("model-update", function(model, k, v)
+            if (model == self.model) then
+                self:modelUpdate(k, v)
+                self:render()
+            end
+        end)
+    end,
+
+    render = function(self)
+        local template = self.template and q(self.template) or nil
+        template = template and template.textContent or nil
+
+        local target = self.target and q(self.target) or nil
+
+        if target then
+            target.innerHTML = lustache:render(template, rawget(self.model, "model"))
+        end
     end
 
 }
 
-function View:render()
-    local template = self.template and q(self.template) or nil
-    template = template and template.textContent or nil
+-- Model
+local Model = Class {
+    
+    init = function(self, options)
+        rawset(self, "model", options)
+    end,
 
-    local target = self.target and q(self.target) or nil
+    __newindex = function(self, k, v)
+        rawset(self.model, k, v)
+        Signal.emit("model-update", self, k, v)
+    end,
 
-    if target then
-        target.innerHTML = lustache:render(template, self.model)
+    __index = function(self, k)
+        return rawget(self.model, k)
+    end,
+
+    __len = function(self)
+        return rawlen(self.model)
     end
-end
+}
 
 return {
-    View = View
+    View = View,
+    Model = Model
 }
--- src/test.lua
+++ src/test.lua
@@ -3,13 +3,18 @@ package.path = "lua_modules/share/lua/5.3/?.lua;lua_modules/share/lua/5.3/?/init
 local moonview = require("moonview")
 
 local View = moonview.View
+local Model = moonview.Model
 
-local v = View({
+local m = Model {
+    name = "John"
+}
+
+local v = View {
     target   = "#content",
     template = "#content-template",
-    model    = {
-        name = "John"
-    }
-})
+    model    = m
+}
 
 v:render()
+
+m.name = "Peter"
